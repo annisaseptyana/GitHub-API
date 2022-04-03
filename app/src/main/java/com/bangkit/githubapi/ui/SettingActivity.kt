84186d736a00c62a959a08498c567b710815631a
annisaseptyana <starrynyteskye@gmail.com>
2022-04-03 20:56:00 +0700

    Applied DataStore to SwitchTheme

--- app/src/main/java/com/bangkit/githubapi/ui/SettingActivity.kt
+++ app/src/main/java/com/bangkit/githubapi/ui/SettingActivity.kt
@@ -1,14 +1,20 @@
 package com.bangkit.githubapi.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import com.bangkit.githubapi.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingBinding
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "setting")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,16 +22,23 @@ class SettingActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val switchTheme = binding.switchTheme
+        val pref = SettingPreferences.getInstance(dataStore)
+        val mainViewModel = ViewModelProvider(this, SettingViewModelFactory(pref)).get(
+            SettingViewModel::class.java
+        )
+        mainViewModel.getThemeSetting().observe(this) {
+                isDarkModeActive: Boolean ->
+                    if (isDarkModeActive) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        switchTheme.isChecked = true
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        switchTheme.isChecked = false
+                    }
+        }
 
         switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
-            if (isChecked) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                switchTheme.isChecked = true
-            }
-            else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                switchTheme.isChecked = false
-            }
+            mainViewModel.saveThemeSetting(isChecked)
         }
     }
 }
--- app/src/main/java/com/bangkit/githubapi/ui/SettingViewModel.kt
+++ app/src/main/java/com/bangkit/githubapi/ui/SettingViewModel.kt
@@ -1,4 +1,21 @@
 package com.bangkit.githubapi.ui
 
-class SettingViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class SettingViewModel (private val pref:SettingPreferences):
+ViewModel(){
+
+    fun getThemeSetting(): LiveData<Boolean> {
+        return pref.getThemeSetting().asLiveData()
+    }
+
+    fun saveThemeSetting(isDarkModeActive: Boolean) {
+        viewModelScope.launch {
+            pref.saveThemeSetting(isDarkModeActive)
+        }
+    }
 }
