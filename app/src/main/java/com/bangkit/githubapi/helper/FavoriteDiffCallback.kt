19ebe38e2292e1829145653180dc310ab856d03f
annisaseptyana <starrynyteskye@gmail.com>
2022-04-03 11:14:25 +0700

    Added UI for Favorites

--- app/src/main/java/com/bangkit/githubapi/helper/FavoriteDiffCallback.kt
+++ app/src/main/java/com/bangkit/githubapi/helper/FavoriteDiffCallback.kt
@@ -1,4 +1,26 @@
 package com.bangkit.githubapi.helper
 
-class FavoriteDiffCallback {
+import androidx.recyclerview.widget.DiffUtil
+import com.bangkit.githubapi.entity.Favorite
+
+class FavoriteDiffCallback (
+    private val mOldFavoriteList: List<Favorite>,
+    private val mNewFavoriteList: List<Favorite>
+        ): DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return mOldFavoriteList.size
+    }
+    override fun getNewListSize(): Int {
+        return mNewFavoriteList.size
+    }
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return mOldFavoriteList[oldItemPosition].id == mNewFavoriteList[newItemPosition].id
+    }
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldUser = mOldFavoriteList[oldItemPosition]
+        val newUser = mNewFavoriteList[newItemPosition]
+        return oldUser.login == newUser.login
+                && oldUser.url == newUser.url
+                && oldUser.avatar_url == newUser.avatar_url
+    }
 }
---
+++ app/src/main/java/com/bangkit/githubapi/ui/FavoriteActivity.kt
@@ -0,0 +1,16 @@
+package com.bangkit.githubapi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.githubapi.databinding.ActivityFavoriteBinding
+
+class FavoriteActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityFavoriteBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFavoriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
